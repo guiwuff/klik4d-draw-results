d3ee6a2209950a007b483fcbc3ce2b5ee1379001
GUI Wuff <gui.wuff@gmail.com>
2017-04-14 10:45:03 +0700

    Initial plugin files

---
+++ klik4d-draw-results/klik4d-draw-results.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Klik4D Draw Results Plugin
+Plugin URI:  https://github.com/guiwuff/klik4d-draw-results
+Description: A Wordpress plugin to fetch and store Klik4D Draw Results. Displayed via template tags.
+Version:     0.1.0-alpha
+Author:      GUI Wuff <gui.wuff@gmail.com>
+Author URI:  https://github.com/guiwuff/
+License:     MIT	
+License URI: https://github.com/guiwuff/klik4d-draw-results/blob/master/LICENSE
+Text Domain: klik4d
+Domain Path: /languages
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
